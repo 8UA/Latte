f31d34673361fe6ee5cef9e6174fd42859a23d77
8UA <succducc0@gmail.com>
2023-11-18 15:34:20 +0100

    + New command

---
+++ commands/music/misc/lyrics.js
@@ -0,0 +1,10 @@
+module.exports = [{
+    name: "lyrics",
+    code: `
+    $color[$random[000000;999999]]
+    $title[$jsonRequest[https://api.popcat.xyz/lyrics?song=$cropText[$message;12];title;]]
+    $description[$jsonRequest[https://api.popcat.xyz/lyrics?song=$cropText[$message;12];lyrics;]]
+    $footer[$jsonRequest[https://api.popcat.xyz/lyrics?song=$cropText[$message;12];artist;]]
+    `,
+  },
+];
